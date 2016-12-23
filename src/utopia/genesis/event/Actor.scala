5219c7d8638d6d826d8a5a17d43939e18d7881cf
Mikkomario <mikko_hilpinen@hotmail.com>
2016-12-23 12:32:19 +0200

    Actor trait added

---
+++ src/utopia/genesis/event/Actor.scala
@@ -0,0 +1,18 @@
+package utopia.genesis.event
+
+/**
+ * Actors are instances that are called between certain intervals to perform some logical, 
+ * (duration based) operations.
+ * @author Mikko Hilpinen
+ * @since 23.12.2016
+ */
+trait Actor
+{
+    /**
+     * This method will be called at a short intervals (usually >= 60 times a second). The instance
+     * should update it's status here. This method should only be called when the object's handling
+     * state allows it.
+     * @param durationMillis The amount of milliseconds passed since the last act call
+     */
+    def act(durationMillis: Double)
+}
