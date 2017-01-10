86dbb68674034083bc6c2fb5074b47a06618a1fd
Mikkomario <mikko_hilpinen@hotmail.com>
2017-01-10 19:04:54 +0200

    Fixed some warnings. Adding new data types.

---
+++ src/utopia/genesis/generic/GenesisDataType.scala
@@ -0,0 +1,47 @@
+package utopia.genesis.generic
+
+import utopia.genesis.util.Vector3D
+import utopia.flow.generic.DataType
+import utopia.genesis.util.Line
+import utopia.genesis.util.Circle
+import utopia.flow.generic.AnyType
+
+/**
+ * Vectors are used for representing motion, force and coordinates
+ */
+object Vector3DType extends DataType("Vector3D", classOf[Vector3D])
+/**
+ * Lines are geometric 3D shapes that have a start and an end point
+ */
+object LineType extends DataType("Line", classOf[Line])
+/**
+ * Circles are geometric shapes that have an origin and a radius
+ */
+object CircleType extends DataType("Circle", classOf[Circle])
+
+/**
+ * This class is used for introducing and managing Genesis-specific data types
+ * @author Mikko Hilpinen
+ * @since 10.1.2017 
+ */
+object GenesisDataType
+{
+    private var isSetup = false
+    
+    /**
+     * Sets up the Genesis-specific data type features, as well as the Flow data type features. 
+     * This method should be called before using any of the data types introduced in this project.
+     */
+    def setup() = 
+    {
+        isSetup = true
+        
+        DataType.setup()
+        DataType.introduceTypes(Vector3DType, LineType, CircleType)
+    }
+}
+
+sealed trait GenesisDataType
+{
+    // TODO: Throw error if the data types have not been set up properly
+}
