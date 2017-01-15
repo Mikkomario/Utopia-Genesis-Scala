86ff3a1cb3add6cdaf46f492bd1e765efee4de90
Mikkomario <mikko_hilpinen@hotmail.com>
2017-01-15 12:06:46 +0200

    Finished the mouse move event
    Movement event types were removed and utility methods were added
    instead.

--- src/utopia/genesis/event/MouseMoveEvent.scala
+++ src/utopia/genesis/event/MouseMoveEvent.scala
@@ -4,10 +4,11 @@ import utopia.genesis.util.Vector3D
 import utopia.inception.event.Event
 import utopia.flow.datastructure.immutable.Model
 import utopia.flow.datastructure.immutable.Value
+import utopia.genesis.generic.GenesisValue
 
 object MouseMoveEvent
 {
-    val eventTypeAttName = "type"
+    val previousPositionAttName = "position_previous"
     val mousePositionAttName = "position"
     val durationMillisAttName = "duration"
 }
@@ -17,11 +18,44 @@ object MouseMoveEvent
  * @author Mikko Hilpinen
  * @since 10.1.2017
  */
-class MouseMoveEvent(val eventType: MouseMoveEventType, val mousePosition: Vector3D, 
-        val durationMillis: Double) extends Event
+class MouseMoveEvent(val mousePosition: Vector3D, 
+        val previousMousePosition: Vector3D, val durationMillis: Double) extends Event
 {
-    // TODO: Add mouse position after vector3D is a suitable value type
-    override val identifiers = Model.apply(Vector(
-            (MouseMoveEvent.eventTypeAttName, Value of eventType.rawValue), 
+    // ATTRIBUTES    --------------------
+    
+    override lazy val identifiers = Model(Vector(
+            (MouseMoveEvent.previousPositionAttName, GenesisValue of previousMousePosition), 
+            (MouseMoveEvent.mousePositionAttName, GenesisValue of mousePosition), 
             (MouseMoveEvent.durationMillisAttName, Value of durationMillis)));
+    
+    
+    // OTHER METHODS    -----------------
+    
+    /**
+     * Checks whether the mouse position is currently over a specified area
+     * @param containment The function that defines whether a specific point is within an area or
+     * not
+     */
+    def isOverArea(containment: Vector3D => Boolean) = containment(mousePosition)
+    
+    /**
+     * Checks whether the mouse position was previously over a specified area
+     * @param containment The function that defines whether a specific point is within an area or
+     * not
+     */
+    def wasOverArea(containment: Vector3D => Boolean) = containment(previousMousePosition)
+    
+    /**
+     * Checks whether the mouse cursor just entered a specified area
+     * @param containment The function that defines whether a specific point is within an area or
+     * not
+     */
+    def enteredArea(containment: Vector3D => Boolean) = !wasOverArea(containment) && isOverArea(containment)
+    
+    /**
+     * Checks whether the mouse cursor just exited a specified area
+     * @param containment The function that defines whether a specific point is within an area or
+     * not
+     */
+    def exitedArea(containment: Vector3D => Boolean) = wasOverArea(containment) && !isOverArea(containment)
 }
