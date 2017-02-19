ac7f403bd2a35a51e6223e047bcc0cf43b081181
Mikkomario <mikko_hilpinen@hotmail.com>
2017-02-19 19:51:02 +0200

    Refactored a bit. Added mouse wheel event.

--- src/utopia/genesis/event/MouseButtonStateListener.scala
+++ src/utopia/genesis/event/MouseButtonStateListener.scala
@@ -4,15 +4,6 @@ import utopia.inception.handling.Handleable
 import utopia.inception.util.AnyFilter
 import utopia.inception.util.Filter
 
-object MouseButtonStateListener
-{
-    /**
-     * The event filter used by the mouse button listeners by default. This filter accepts 
-     * all mouse button events.
-     */
-    val defaultEventFilter = new AnyFilter[MouseButtonStateEvent]()    
-}
-
 /**
  * This trait is extended by classes which are interested in changes in mouse button statuses
  * @author Mikko Hilpinen
@@ -31,6 +22,5 @@ trait MouseButtonStateListener extends Handleable
      * The filter applied to the incoming mouse button events. The listener will only be informed
      * about the events accepted by the filter. The default filter accepts all mouse button events.
      */
-    def mouseButtonStateEventFilter: Filter[MouseButtonStateEvent] = 
-            MouseButtonStateListener.defaultEventFilter;
+    def mouseButtonStateEventFilter: Filter[MouseButtonStateEvent] = AnyFilter
 }
---
+++ src/utopia/genesis/event/MouseWheelEvent.scala
@@ -0,0 +1,13 @@
+package utopia.genesis.event
+
+import utopia.genesis.util.Vector3D
+
+/**
+ * These mouse events are generated whenever the mouse wheel turns
+ * @author Mikko Hilpinen
+ * @since 19.2.2017
+ * @param wheelTurn The amount of mouse wheel 'notches' since the last event. A positive number
+ * indicates a wheel turn towards the user. A negative number indicates a roll away from the user.
+ */
+class MouseWheelEvent(val wheelTurn: Double, mousePosition: Vector3D, buttonStatus: MouseButtonStatus) 
+        extends MouseEvent(mousePosition, buttonStatus)
