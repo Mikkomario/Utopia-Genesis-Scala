d060600f9d7eb9f01cc8a1205f330866e61cdc5b
Mikkomario <mikko_hilpinen@hotmail.com>
2017-02-22 08:00:02 +0200

    Working on the new keyState handling

---
+++ src/utopia/genesis/view/WindowKeyEventGenerator.scala
@@ -0,0 +1,14 @@
+package utopia.genesis.view
+
+import java.awt.Window
+
+/**
+ * This class listens to key events within a specific window and generates keyboard events based on
+ * the java.awt counterparts
+ * @author Mikko Hilpinen
+ * @since 22.2.2017
+ */
+class WindowKeyEventGenerator(window: Window)
+{
+    
+}
