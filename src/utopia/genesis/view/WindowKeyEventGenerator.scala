752093f016256de59176326e5112b25433bd068a
Mikkomario <mikko_hilpinen@hotmail.com>
2017-02-22 08:05:54 +0200

    Working on key event generator

--- src/utopia/genesis/view/WindowKeyEventGenerator.scala
+++ src/utopia/genesis/view/WindowKeyEventGenerator.scala
@@ -1,6 +1,8 @@
 package utopia.genesis.view
 
 import java.awt.Window
+import java.awt.event.KeyListener
+import java.awt.event.KeyEvent
 
 /**
  * This class listens to key events within a specific window and generates keyboard events based on
@@ -10,5 +12,12 @@ import java.awt.Window
  */
 class WindowKeyEventGenerator(window: Window)
 {
+    // NESTED CLASSES    -----------------
     
+    private class KeyEventReceiver extends KeyListener
+    {
+        override def keyPressed(e: KeyEvent) = Unit
+        override def keyReleased(e: KeyEvent) = Unit
+        override def keyTyped(e: KeyEvent) = Unit
+    }
 }
