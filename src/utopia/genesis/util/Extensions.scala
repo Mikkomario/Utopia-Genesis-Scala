b6ab9d3dc2b5a70d8e2958afd12e3c30ca496415
Mikkomario <mikko_hilpinen@hotmail.com>
2017-07-22 11:38:06 +0300

    Moved some features from conflict to genesis

--- src/utopia/genesis/util/Extensions.scala
+++ src/utopia/genesis/util/Extensions.scala
@@ -30,6 +30,35 @@ object Extensions
         }
     }
     
+    implicit class FailableIterable[T](val c: Iterable[T]) extends AnyVal
+    {
+        /**
+         * This function maps values like a normal map function, but terminates immediately if 
+         * None is returned by the transform function
+         * @return The mapped collection or none if mapping failed for any element
+         */
+        def mapOrFail[B](f: T => Option[B]): Option[Vector[B]] = 
+        {
+            val iterator = c.iterator
+            val buffer = Vector.newBuilder[B]
+            
+            while (iterator.hasNext)
+            {
+                val result = f(iterator.next())
+                if (result.isDefined)
+                {
+                    buffer += result.get
+                }
+                else
+                {
+                    return None
+                }
+            }
+            
+            Some(buffer.result())
+        }
+    }
+    
     @deprecated("Replaced with the new drawer class", "v0.3")
     implicit class ExtendedGraphics(val g: Graphics2D) extends AnyVal
     {
--- src/utopia/genesis/util/Projectable.scala
+++ src/utopia/genesis/util/Projectable.scala
@@ -1,5 +1,7 @@
 package utopia.genesis.util
 
+import utopia.genesis.util.Extensions._
+
 /**
  * This trait is extended by shapes that can be projected over a specified axis
  * @author Mikko Hilpinen
@@ -17,14 +19,18 @@ trait Projectable
     
     // OTHER METHODS    -----------------------
     
-    // Calculates if / how much the projections of the two shapes overlap on the specified axis
-    // Returns the mtv for the specified axis, if there is overlap
+    /**
+    * Calculates if / how much the projections of the two shapes overlap on the specified axis
+    * @param other the other projectable shape
+    * @param axis the axis along which the overlap is checked
+    * @return the mtv for the specified axis, if there is overlap
+    */
     def projectionOverlapWith(other: Projectable, axis: Vector3D) = 
     {
         val projection = projectedOver(axis)
         val otherProjection = other.projectedOver(axis)
         
-        if (projection.end < otherProjection.start || projection.start > otherProjection.end)
+        if (projection.end <= otherProjection.start || projection.start >= otherProjection.end)
         {
             None
         }
@@ -34,6 +40,30 @@ trait Projectable
             val backwardsMtv = otherProjection.start - projection.end
             
             if (forwardsMtv.length < backwardsMtv.length) Some(forwardsMtv) else Some(backwardsMtv)
+        }
+    }
+    
+    /**
+     * Calculates the minimum translation vector that would get the two projectable shapes out of 
+     * a collision situation
+     * @param other the other projectable instance
+     * @param axes the axes along which the collision is checked
+     * @return The minimum translation vector that gets the two shapes out of a collision situation 
+     * or none if there is no collision
+     */
+    def collisionMtvWith(other: Projectable, axes: Iterable[Vector3D]) = 
+    {
+        // If there is collision, there must be overlap on each axis
+        val mtvs = axes.mapOrFail { projectionOverlapWith(other, _) }
+        
+        if (mtvs.isDefined && !mtvs.get.isEmpty)
+        {
+            // Finds the smallest possible translation vector
+            Some(mtvs.get.minBy { _.length })
+        }
+        else
+        {
+            None
         }
     }
 }
