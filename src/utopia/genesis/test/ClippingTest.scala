ab1ed6086508e6765330f146df15cf5abd8bfdb5
Mikkomario <mikko_hilpinen@hotmail.com>
2019-04-08 20:39:46 +0300

    Clipping test completed

--- src/utopia/genesis/test/ClippingTest.scala
+++ src/utopia/genesis/test/ClippingTest.scala
@@ -4,15 +4,19 @@ import utopia.genesis.util.Drawer
 import utopia.genesis.shape.Vector3D
 import java.awt.Color
 
-import utopia.genesis.shape.shape2D.Line
+import utopia.flow.async.ThreadPool
 import utopia.genesis.view.Canvas
 import utopia.genesis.view.MainFrame
 import utopia.genesis.view.CanvasMouseEventGenerator
 import utopia.genesis.event.MouseMoveEvent
-import utopia.genesis.handling.{Drawable, MouseMoveListener}
-import utopia.genesis.shape.shape2D.Circle
+import utopia.genesis.handling.mutable.{ActorHandler, DrawableHandler, MouseButtonStateHandler, MouseMoveHandler, MouseWheelHandler}
+import utopia.genesis.handling.{ActorLoop, Drawable, MouseMoveListener}
+import utopia.genesis.shape.shape2D.{Circle, Point, ShapeConvertible, Size}
 import utopia.genesis.util.DepthRange
-import utopia.genesis.shape.shape2D.ShapeConvertible
+import utopia.inception.handling.immutable.Handleable
+import utopia.inception.handling.mutable.HandlerRelay
+
+import scala.concurrent.ExecutionContext
 
 /**
  * This test tests the drawer's clipping functionality
@@ -20,15 +24,13 @@ import utopia.genesis.shape.shape2D.ShapeConvertible
  * @since 25.2.2017
  */
 object ClippingTest extends App
-{   
-    /* TODO: Return and fix code after refactoring is done
-    class HiddenShapeDrawer(val shapes: Iterable[ShapeConvertible]) 
-            extends Drawable with MouseMoveListener
+{
+    class HiddenShapeDrawer(val shapes: Iterable[ShapeConvertible]) extends Drawable with MouseMoveListener with Handleable
     {
-        override val depth = DepthRange.foreground
+        override val drawDepth = DepthRange.foreground
         
         private val clipRadius = 64
-        private var clip = Circle(Vector3D.zero, clipRadius)
+        private var clip = Circle(Point.origin, clipRadius)
         
         def draw(drawer: Drawer) = 
         {
@@ -40,23 +42,38 @@ object ClippingTest extends App
         
         def onMouseMove(event: MouseMoveEvent) = clip = Circle(event.mousePosition, clipRadius)
     }
+	
+	// Creates the handlers
+    val worldSize = Size(800, 600)
     
-    val worldSize = Vector3D(800, 600)
-    val canvas = new Canvas(worldSize, 120)
+	val drawHandler = DrawableHandler()
+	val actorHandler = ActorHandler()
+	val mouseMoveHandler = MouseMoveHandler()
+	val mouseButtonHandler = MouseButtonStateHandler()
+	val mouseWheelHandler = MouseWheelHandler()
+	
+	val handlers = HandlerRelay(drawHandler, actorHandler, mouseMoveHandler, mouseButtonHandler, mouseWheelHandler)
+	
+	// Creates the frame
+	val canvas = new Canvas(drawHandler, worldSize)
     val frame = new MainFrame(canvas, worldSize, "Clipping Test")
     
-    val actorThread = new ActorThread(20, 120)
-    val mouseGen = new CanvasMouseEventGenerator(canvas)
-    actorThread.handler += mouseGen
+	// Creates event generators
+    val actorLoop = new ActorLoop(actorHandler, 20 to 120)
+    val mouseGen = new CanvasMouseEventGenerator(canvas, mouseMoveHandler, mouseButtonHandler, mouseWheelHandler)
     
-    val handlers = new HandlerRelay(actorThread.handler, canvas.handler, mouseGen.moveHandler)
-    
-    handlers += new GridDrawer(worldSize, Vector3D(64, 64))
+	actorHandler += mouseGen
+	
+	// Creates test objects
+    handlers += new GridDrawer(worldSize, Size(64, 64))
     handlers += new HiddenShapeDrawer(Vector(
-            Circle(worldSize / 2, 32), Circle(worldSize * 0.7, 64), 
-            Circle(worldSize.xProjection + Vector3D(- 64, 64), 32)));
+            Circle((worldSize / 2).toPoint, 32), Circle((worldSize * 0.7).toPoint, 64),
+            Circle((worldSize.xProjection + Vector3D(- 64, 64)).toPoint, 32)))
     
-    actorThread.start()
+	// Starts the program
+	implicit val context: ExecutionContext = new ThreadPool("Test").executionContext
+	
+	canvas.startAutoRefresh()
+	actorLoop.startAsync()
     frame.display()
-    * */
 }
