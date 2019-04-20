ea149cc191f97b6268bd851ece8fb3688b0249f7
Mikkomario <mikko_hilpinen@hotmail.com>
2019-04-20 19:01:42 +0300

    Tested shapes and collision detection

--- src/utopia/genesis/test/ShapeTest.scala
+++ src/utopia/genesis/test/ShapeTest.scala
@@ -1,22 +1,18 @@
 package utopia.genesis.test
 
-import utopia.genesis.shape.shape2D.Line
 import utopia.genesis.shape.Vector3D
-import utopia.genesis.shape.shape2D.Circle
 import utopia.genesis.generic.GenesisDataType
+import utopia.genesis.shape.shape2D.{Circle, Line, Point}
 
 /**
  * This test is for some intersection methods and other shape (line, circle) specific methods
  */
 object ShapeTest extends App
 {
-    /* TODO: Return and fix code after refactoring is done
     GenesisDataType.setup()
     
-    val line1 = Line(Vector3D.zero, Vector3D(10))
-    val line2 = Line(Vector3D(0, 1), Vector3D(2, -1))
-    val line3 = Line(Vector3D(0, 0, 1), Vector3D(10, 0, 1))
-    val line4 = Line(Vector3D(0, 0, 2), Vector3D(10, 0, -2))
+    val line1 = Line(Point.origin, Point(10, 0))
+    val line2 = Line(Point(0, 1), Point(2, -1))
     
     assert(line1(0) ~== line1.start)
     assert(line1(1) ~== line1.end)
@@ -24,35 +20,30 @@ object ShapeTest extends App
     val intersection12 = line1 intersection line2
     
     assert(intersection12.isDefined)
-    assert(intersection12.get ~== Vector3D(1, 0))
+    assert(intersection12.get ~== Point(1, 0))
     
-    assert(line1.intersection(line3).isEmpty)
-    assert(line1.intersection(line4).isDefined)
-    
-    val line5 = Line(Vector3D(1, 2, 0), Vector3D(1, 1, 0))
+    val line5 = Line(Point(1, 2), Point(1, 1))
     assert(line1.intersection(line5, false).isDefined)
-    assert(line1.intersection(line5, true).isEmpty)
-    assert(line5.intersection(line1, true).isEmpty)
+    assert(line1.intersection(line5).isEmpty)
+    assert(line5.intersection(line1).isEmpty)
     
-    val circle1 = Circle(Vector3D(3), 2)
+    val circle1 = Circle(Point(3, 0), 2)
     
     assert(line1.circleIntersection(circle1, false).size == 2)
     assert(line2.circleIntersection(circle1).size == 1)
     assert(line5.circleIntersection(circle1).isEmpty)
     assert(line5.circleIntersection(circle1, false).size == 1)
     
-    val circle2 = Circle(Vector3D(4), 1)
+    val circle2 = Circle(Point(4, 0), 1)
     
     assert(circle1 contains circle2)
     assert(!circle2.contains(circle1))
-    assert(!circle1.contains(Circle(Vector3D(5), 1)))
+    assert(!circle1.contains(Circle(Point(5, 0), 1)))
     
     
     // Tests line clipping
-    assert(line1.clipped(Vector3D(5), Vector3D(1)).get == Line(Vector3D(5), Vector3D(10)))
-    assert(line1.clipped(Vector3D(5, 2), Vector3D(-1)).get == Line(Vector3D(0), Vector3D(5)))
-    assert(line1.clipped(Vector3D(-2, -2), Vector3D(-1)) == None)
-    assert(line1.clipped(Vector3D(1, 1), Vector3D(1, 1)).get == Line(Vector3D(2), Vector3D(10)))
-    
-    println("Success")*/
+    assert(line1.clipped(Point(5, 0), Vector3D(1)).get == Line(Point(5, 0), Point(10, 0)))
+    assert(line1.clipped(Point(5, 2), Vector3D(-1)).get == Line(Point.origin, Point(5, 0)))
+    assert(line1.clipped(Point(-2, -2), Vector3D(-1)).isEmpty)
+    assert(line1.clipped(Point(1, 1), Vector3D(1, 1)).get == Line(Point(2, 0), Point(10, 0)))
 }
