196fbb98e82d60157707d192bc04457cc8fd3cc8
Mikkomario <mikko_hilpinen@hotmail.com>
2019-04-21 12:42:18 +0300

    Added more functional listeners

--- src/utopia/genesis/handling/MouseWheelListener.scala
+++ src/utopia/genesis/handling/MouseWheelListener.scala
@@ -1,8 +1,29 @@
 package utopia.genesis.handling
 
 import utopia.genesis.event.MouseWheelEvent
+import utopia.genesis.shape.shape2D.Area2D
 import utopia.inception.handling.Handleable
 import utopia.inception.util.{AnyFilter, Filter}
+
+object MouseWheelListener
+{
+    /**
+      * Creates a new mouse wheel listener that calls specified function on wheel rotations
+      * @param f A function that will be called on wheel rotations
+      * @param filter A filter that specifies, which events will trigger the function (default = no filtering)
+      * @return A new mouse event listener
+      */
+    def apply(f: MouseWheelEvent => Unit, filter: Filter[MouseWheelEvent] = AnyFilter): MouseWheelListener =
+        new FunctionalMouseWheelListener(f, filter)
+    
+    /**
+      * Creates a new mouse wheel listener that calls specified function on wheel rotations inside a specific area
+      * @param getArea A function for getting the target area
+      * @param f A function that will be called on wheel rotations
+      * @return A new mouse event listener
+      */
+    def onWheelInsideArea(getArea: => Area2D, f: MouseWheelEvent => Unit) = apply(f, e => e.isOverArea(getArea))
+}
 
 /**
  * This trait is implemented by classes which are interested in being notified when the mouse wheel
@@ -25,4 +46,12 @@ trait MouseWheelListener extends Handleable
       * @return Whether this instance is willing to receive mouse wheel events
       */
     def isReceivingMouseWheelEvents = allowsHandlingFrom(MouseWheelHandlerType)
+}
+
+private class FunctionalMouseWheelListener(val f: MouseWheelEvent => Unit, val filter: Filter[MouseWheelEvent])
+    extends MouseWheelListener with utopia.inception.handling.immutable.Handleable
+{
+    override def onMouseWheelRotated(event: MouseWheelEvent) = f(event)
+    
+    override def mouseWheelEventFilter = filter
 }
