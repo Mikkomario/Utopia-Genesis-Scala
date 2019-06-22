9b66560a3e13ce342dc40ee0c374a42aed0a11e6
Mikkomario <mikko_hilpinen@hotmail.com>
2019-06-22 11:54:28 +0300

    Flow: Added some collection extensions Genesis: Fixed compound bezier paths

--- src/utopia/genesis/shape/path/BezierPath.scala
+++ src/utopia/genesis/shape/path/BezierPath.scala
@@ -1,13 +1,12 @@
 package utopia.genesis.shape.path
 
+import utopia.flow.util.CollectionExtensions._
 import utopia.genesis.util.Arithmetic
 
-import scala.collection.immutable.VectorBuilder
-
 /**
-  * Bezier paths consists of multiple cubic bezier parts, each of which is connected smoothly with each other
+  * This class calculates a smooth path between points that consists of bezier curves
   * @author Mikko Hilpinen
-  * @since 19.6.2019, v2.1+
+  * @since 22.6.2019, v2.1+
   */
 object BezierPath
 {
@@ -27,115 +26,51 @@ object BezierPath
 		else if (points.size == 2)
 			LinearPath(points.head, points(1))
 		else
-			CompoundPath(calculateBezier(points))
+			CompoundPath(calculatePath(points).toVector)
 	}
 	
-	// NB: The code in this function is heavily based on: https://www.stkent.com/2015/07/03/building-smooth-paths-using-bezier-curves.html
-	private def calculateBezier[P <: Arithmetic[P, P]](points: Vector[P]): Vector[CubicBezier[P]] =
+	private def calculatePath[P <: Arithmetic[P, P]](points: Seq[P]) =
 	{
-		val start = points.head
-		val end = points.last
-		val curveAmount = points.size - 1
+		// Number of curves
+		val n = points.size - 1
 		
-		def baseTarget(index: Int) =
+		// Base target consists of three parts: Beginning (p0 + 2p1), middle ((2pn + pn+1)*2) and end (8pn-1 + pn)
+		val baseTarget = (points.head + points(1) * 2) +:
+			(1 until n - 1).map { i => (points(i) * 2 + points(i + 1)) * 2 } :+ points(n - 1) * 8 + points(n)
+		
+		// Lower diagonal has length n-1 and consists of 1's followed by a 2
+		val lowerDiagonal = Vector.fill(n - 2)(1.0) :+ 2.0
+		
+		// Main diagonal consists of 2 followed by 4s and ending with 7. Length = n
+		val mainDiagonal = 2.0 +: Vector.fill(n - 2)(4.0) :+ 7.0
+		
+		// Basic upper diagonal only consists of 1's. Length = n-1
+		val baseUpperDiagonal = Vector.fill(n - 1)(1.0)
+		
+		// Calculates the real upper diagonal. Each value requires the previous one.
+		val firstUpperDiagonal = baseUpperDiagonal.head / mainDiagonal.head
+		val upperDiagonal = (1 until (n - 1)).foldMapLeft(firstUpperDiagonal) { (last, i) =>
+			baseUpperDiagonal(i) / (mainDiagonal(i) - lowerDiagonal(i - 1) * last) }
+		
+		// Calculates the real target. Again, each value is dependent from the one before
+		val firstTarget = baseTarget.head / mainDiagonal.head
+		val target = (1 until n).foldMapLeft(firstTarget)
 		{
-			// Case: First point
-			if (index < 1)
-				start + (points(1) * 2)
-			// Case: One of middle points
-			else if (index < curveAmount - 1)
-				(points(index) * 2 + points(index + 1)) * 2
-			// Case: Final point
-			else
-				points(curveAmount - 1) * 8 + points(curveAmount)
+			(last, i) =>
+				val scale = 1 / (mainDiagonal(i) - lowerDiagonal(i - 1) * upperDiagonal(i - 1))
+				(baseTarget(i) - last * lowerDiagonal(i - 1)) * scale
 		}
 		
-		def lowerDiagonal(index: Int): Double = if (index < curveAmount - 1) 1 else 2
+		// Calculates the first control points from end to beginning
+		val lastControl1 = target.last
+		val controlPoints1 = ((n - 2) to 0 by -1).foldMapLeft(lastControl1) { (next, i) =>
+			target(i) - next * upperDiagonal(i) }.reverse
 		
-		def mainDiagonal(index: Int): Double = if (index < 1) 2 else if (index < curveAmount - 1) 2 else 7
+		// Calculates the second control points from the first (last one calculated separately)
+		val lastControl2 = (points.last + lastControl1) / 2
+		val controlPoints2 = (0 until (n - 1)).map { i => points(i + 1) * 2 - controlPoints1(i + 1) } :+ lastControl2
 		
-		// Calculates upper diagonals
-		val upperDiagonals =
-		{
-			val upperDiagonalBuffer = new VectorBuilder[Double]
-			
-			// Adds first point
-			var lastNewUpperDiagonal = 1 / mainDiagonal(0)
-			upperDiagonalBuffer += lastNewUpperDiagonal
-			
-			// Adds the rest of the points
-			(1 until (curveAmount - 1)).foreach
-			{
-				i =>
-					lastNewUpperDiagonal = 1 / (mainDiagonal(i) - lowerDiagonal(i - 1) * lastNewUpperDiagonal)
-					upperDiagonalBuffer += lastNewUpperDiagonal
-			}
-			
-			upperDiagonalBuffer.result()
-		}
-		
-		// Calculates targets
-		val targets =
-		{
-			val targetBuffer = new VectorBuilder[P]
-			
-			// Adds the first point
-			var lastTarget = baseTarget(0) * (1 / mainDiagonal(0))
-			targetBuffer += lastTarget
-			
-			// Adds rest of the points
-			(1 until curveAmount).foreach
-			{
-				i =>
-					val targetScale = 1 / (mainDiagonal(i) - lowerDiagonal(i - 1) * upperDiagonals(i - 1))
-					lastTarget = (baseTarget(i) - (lastTarget * lowerDiagonal(i - 1))) * targetScale
-					targetBuffer += lastTarget
-			}
-			
-			targetBuffer.result()
-		}
-		
-		// Calculates the first set of control points
-		val firstControlPoints =
-		{
-			// Last point (The points are added in reverse order)
-			val firstControlPointsBuffer = new VectorBuilder[P]
-			var lastControlPoint = targets.last
-			firstControlPointsBuffer += lastControlPoint
-			
-			// Rest of the points
-			(curveAmount - 2 to 0 by -1).foreach
-			{
-				i =>
-					lastControlPoint = targets(i) - (lastControlPoint * upperDiagonals(i))
-					firstControlPointsBuffer += lastControlPoint
-			}
-			
-			firstControlPointsBuffer.result().reverse
-		}
-		
-		// Calculates all control points
-		val controlPoints =
-		{
-			// Calculates the second half of control points (last point is added separately)
-			val secondControlPointsBuffer = new VectorBuilder[P]
-			(0 until firstControlPoints.size - 1).foreach { i => secondControlPointsBuffer +=
-				points(i + 1) * 2 - firstControlPoints(i + 1) }
-			secondControlPointsBuffer += (end + firstControlPoints.last) / 2
-			
-			// Returns the paired control points
-			firstControlPoints.zip(secondControlPointsBuffer.result())
-		}
-		
-		// Calculates the actual curves based on control points
-		(0 until curveAmount).map
-		{
-			i =>
-				val curveStart = points(i)
-				val curveEnd = points(i + 1)
-				val (c1, c2) = controlPoints(i)
-				
-				CubicBezier(curveStart, c1, c2, curveEnd)
-		}.toVector
+		// Creates the curves
+		(0 until n).map { i => CubicBezier(points(i), controlPoints1(i), controlPoints2(i), points(i + 1)) }
 	}
 }
